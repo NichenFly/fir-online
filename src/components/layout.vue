b4b7bdc5e0d35cd1085b6282cd899c9040a2bbff
朱良 <zhuliang@int-yt.com>
2017-11-30 22:33:30 +0800

    引入iview

---
+++ src/components/layout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>哎呀, 默宸</p>
+        <Button @click="show">Click me!</Button>
+        <Modal v-model="visible" title="Welcome">Welcome to iView</Modal>
+    </div>
+</template>
+<script>
+import { Button, Modal } from 'iview'
+export default {
+    data() {
+        return {
+            visible: false
+        }
+    },
+    methods: {
+        show() {
+            this.visible = true
+        }
+    },
+    components: {
+        Button, Modal
+    }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
